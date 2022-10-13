5a3e1afbb9c79142f1b4d29fbc58583a5ca24cd9
shimashimau <shuriken233@gmail.com>
2022-10-13 10:21:39 +0200

    restyling

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -1,6 +1,19 @@
+const Services = () => {
 
-const Services = ()=>{
+    return (
+        <section className="services-page wrapper">
+            <article className="intro page-intro">
+                <div className="text">
+                    <h1 className="headline text-center">Våra tjänster</h1>
+                    <h4 className="text-center">Renax AB är ett städföretag som har länge erbjudit industriledande och
+                        miljöcertifierad städservice inom industristädning, fabriksstädning, livsmedelsstädning och
+                        kontorsstädning.
 
-    return <h1>Services</h1>
+                        Vi är verksamma i hela Sverige och anställer idag runt 100 anställda med uppdrag åt stora
+                        livsmedelsföretag och mindre lokalvård.</h4>
+                </div>
+            </article>
+        </section>
+    )
 }
 export default Services;
